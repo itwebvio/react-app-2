9351cdebf728b487806d80f011f401c38c6e3926
itwebvio <it.webvio@gmail.com>
2025-08-05 17:15:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '100px' }}>
-      <h1>Hello from React App 2!</h1>
+      <h1>Hello from React App 2!!</h1>
     </div>
   );
 }
